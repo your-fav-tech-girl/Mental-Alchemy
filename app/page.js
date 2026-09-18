11c6ca9a483b1cd6dc08d566ad75808fc2901fd1
funmilola <funmilolaakanbi44@gmail.com>
2026-09-19 03:28:52 +0400

    changed page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,7 @@
 import LegacyPage from '@/components/LegacyPage'
 
 export const dynamic = 'force-static'
-export const metadata = { title: 'Contact' }
 
-export default function ContactPage() {
-  return <LegacyPage name="contact" />
+export default function HomePage() {
+  return <LegacyPage name="home" />
 }
